357cae15c51ded948ca5a5c1c8bdb95fb91261b9
JULIOCT <juliocejudo@outlook.com>
2020-03-01 19:49:49 +0100

    tests

--- UcemeTests/Utilidades/NotificacionesTests.cs
+++ UcemeTests/Utilidades/NotificacionesTests.cs
@@ -1,12 +1,6 @@
 ﻿namespace UCEME.Utilidades.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using UCEME.Utilidades;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestClass()]
     public class NotificacionesTests
@@ -16,43 +10,43 @@
         [TestMethod()]
         public void SendPasswordRetrievalTest()
         {
-            Utilidades.Notificaciones.SendPasswordRetrieval(emailTo, "AAA");
-            Assert.Fail();
+            var sut = Utilidades.Notificaciones.SendPasswordRetrieval(emailTo, "AAA");
+            Assert.IsTrue(sut);
         }
 
         [TestMethod()]
         public void SendNewUserCreatedTest()
         {
-            Utilidades.Notificaciones.SendNewUserCreated(emailTo, "AAA");
-            Assert.Fail();
+            var sut = Utilidades.Notificaciones.SendNewUserCreated(emailTo, "AAA");
+            Assert.IsTrue(sut);
         }
 
         [TestMethod()]
         public void EnviarCorreoContactoTest()
         {
-            Utilidades.Notificaciones.EnviarCorreoContacto(emailTo, "AAA", "BBB");
-            Assert.Fail();
+            var sut = Utilidades.Notificaciones.EnviarCorreoContacto(emailTo, "AAA", "BBB");
+            Assert.IsTrue(sut);
         }
 
         [TestMethod()]
         public void ModificarCitasMedicosTest()
         {
-            Utilidades.Notificaciones.ModificarCitasMedicos(new Models.Cita());
-            Assert.Fail();
+            var sut = Utilidades.Notificaciones.ModificarCitasMedicos(new Models.Cita());
+            Assert.IsTrue(sut);
         }
 
         [TestMethod()]
         public void NotificarCitasMedicosTest()
         {
-            Utilidades.Notificaciones.NotificarCitasMedicos(new Models.Cita(), "AAA");
-            Assert.Fail();
+            var sut = Utilidades.Notificaciones.NotificarCitasMedicos(new Models.Cita(), "AAA");
+            Assert.IsTrue(sut);
         }
 
         [TestMethod()]
         public void NotificarCitasUsuarioTest()
         {
-            Utilidades.Notificaciones.NotificarCitasUsuario(new Models.Cita(), "AAA");
-            Assert.Fail();
+            var sut = Utilidades.Notificaciones.NotificarCitasUsuario(new Models.Cita(), "AAA");
+            Assert.IsTrue(sut);
         }
     }
 }
