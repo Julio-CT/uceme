6879f420df367f110708ed42b5de1a51113f69b3
JulioCT <juliocejudo@outlook.com>
2020-05-20 00:06:37 +0200

    #7 new ui working with react and react bootstrap

---
+++ Uceme.UI/ClientApp/src/App.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+
+it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>, div);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- Uceme.UI/ClientApp/src/App.tsx
+++ Uceme.UI/ClientApp/src/App.tsx
@@ -7,20 +7,19 @@ import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
+import './App.css';
+import './custom.css';
 
-import './custom.css'
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+const displayName = App.name;
+function App() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
-        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      </Layout>
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <AuthorizeRoute path='/fetch-data' component={FetchData} />
+            <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+        </Layout>
     );
-  }
 }
+
+export default App;
