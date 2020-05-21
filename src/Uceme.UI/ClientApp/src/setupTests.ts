627927042294ce5fdadad31d0b01a419de75d1a7
JulioCT <juliocejudo@outlook.com>
2020-05-21 08:39:35 +0200

    #9 some ts issues fixed

--- src/Uceme.UI/ClientApp/src/setupTests.ts
+++ src/Uceme.UI/ClientApp/src/setupTests.ts
@@ -5,25 +5,40 @@
 import '@testing-library/jest-dom/extend-expect';
 
 const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  clear: jest.fn(),
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
 };
-global.localStorage = localStorageMock;
+
+(global as any).localStorage = localStorageMock;
 
 // Mock the request issued by the react app to get the client configuration parameters.
-window.fetch = () => {
-  return Promise.resolve(
-    {
-      ok: true,
-      json: () => Promise.resolve({
-        "authority": "https://localhost:5001",
-        "client_id": "Uceme.UI",
-        "redirect_uri": "https://localhost:5001/authentication/login-callback",
-        "post_logout_redirect_uri": "https://localhost:5001/authentication/logout-callback",
-        "response_type": "id_token token",
-        "scope": "Uceme.UIAPI openid profile"
-     })
-    });
-};
+////window.fetch = () => {
+////  return Promise.resolve(
+////    {
+////      ok: true,
+////      json: () => Promise.resolve({
+////        "authority": "https://localhost:5001",
+////        "client_id": "Uceme.UI",
+////        "redirect_uri": "https://localhost:5001/authentication/login-callback",
+////        "post_logout_redirect_uri": "https://localhost:5001/authentication/logout-callback",
+////        "response_type": "id_token token",
+////        "scope": "Uceme.UIAPI openid profile"
+////      }),
+////    headers: new Headers(),
+////    redirected: false,
+////    status: 1,
+////    statusText: "",
+////    trailer: new Promise<Headers>(void),
+////    type: null,
+////    url: null,
+////    clone: null,
+////    body: null,
+////    bodyUsed: null,
+////    blob: null,
+////    text: null,
+////    arrayBuffer: null,
+////    formData: null
+////    });
+////};
