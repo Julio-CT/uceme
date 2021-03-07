a3630f30baef6acd3ffdcad720dfe56b8a45bb9b
Julio-CT <juliocejudo@outlook.com>
2021-03-07 09:38:21 +0000

    #62B jsonserver handling post requests (#36)

--- src/Uceme.UI/ClientApp/src/App.tsx
+++ src/Uceme.UI/ClientApp/src/App.tsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import BlogHome from './components/BlogHome';
+import ConditionsComponent from './components/ConditionsComponent';
 import BlogPostComponent from './components/BlogPostComponent';
 import SpecialitiesComponent from './components/SpecialitiesComponent';
 import ContactUsComponent from './components/ContactUsComponent';
@@ -15,6 +16,7 @@ function App(): JSX.Element {
   return (
     <Layout>
       <Route exact path="/" component={Home} />
+      <Route path="/condiciones" component={ConditionsComponent} />
       <Route path="/especialidades" component={SpecialitiesComponent} />
       <Route path="/innovaciones" component={Home} />
       <Route path="/blog/:page?" component={BlogHome} />
