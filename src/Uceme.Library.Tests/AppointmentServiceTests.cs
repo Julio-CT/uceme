d2fe55a3639f60c99649200bfdc834c0f55d1759
JTejero <juliocejudo@outlook.com>
2022-10-08 19:18:04 +0200

    ok, maybe it was the tests?

---
+++ src/Uceme.Library.Tests/AppointmentServiceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Uceme.Library.Tests
+{
+    using Microsoft.Extensions.Logging;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Uceme.Library.Services;
+    using Uceme.Model.Data;
+
+    [TestClass]
+    public class AppointmentServiceTests
+    {
+        [TestMethod]
+        public void AppointmentService_RightInput_RightOutput()
+        {
+            //////// ARRANGE
+            ////var logger = new Mock<ILogger<HospitalService>>();
+            ////var context = new Mock<IApplicationDbContext>();
+            ////var emailService = new Mock<IEmailService>();
+            ////var sut = new AppointmentService(
+            ////    logger.Object,
+            ////    context.Object,
+            ////    emailService.Object);
+
+            //////// ACT
+            ////sut.GetAppointments();
+
+            //////// ASSERT
+        }
+    }
+}
