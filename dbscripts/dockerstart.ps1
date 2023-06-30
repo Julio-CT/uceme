dfc61bfacfbf81fb6757ffc08ca6c44049361f0b
Julio-CT <juliocejudo@outlook.com>
2023-07-01 00:42:35 +0200

    problems running docker locally

--- dbscripts/dockerstart.ps1
+++ dbscripts/dockerstart.ps1
@@ -1,5 +1,6 @@
 docker-compose up -d
 
-docker cp ucemedb.sql DockerUcemeDb:/var/opt/mssql/backup
+# docker cp ucemedb.sql DockerUcemeDb:/var/opt/mssql/backup
+# docker cp ucemedb.sql sqlcmd:/var/opt/mssql/backup
 
-docker exec -it "DockerUcemeDb" /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Ch4mp10ns' -i /var/opt/mssql/backup
+# docker exec -it "DockerUcemeDb" /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Ch4mp10ns' -i /var/opt/mssql/backup
