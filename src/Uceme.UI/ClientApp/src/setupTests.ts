e65762120c30a8e60b9261181c81d1915e19d75f
Julio-CT <juliocejudo@outlook.com>
2024-01-05 00:26:48 +0100

    update some packages just for fun

--- src/Uceme.UI/ClientApp/src/setupTests.ts
+++ src/Uceme.UI/ClientApp/src/setupTests.ts
@@ -1,11 +1,11 @@
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  clear: jest.fn(),
-};
+// const localStorageMock = {
+//   getItem: jest.fn(),
+//   setItem: jest.fn(),
+//   removeItem: jest.fn(),
+//   clear: jest.fn(),
+// };
 
-(global as any).localStorage = localStorageMock;
+//(global as any).localStorage = localStorageMock;
 
 // Mock the request issued by the react app to get the client configuration parameters.
 window.fetch = () => {
